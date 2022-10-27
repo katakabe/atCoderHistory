442371ed470ab6ca43d9089d6cd65b5e037e6461
katakabe <76699824+katakabe@users.noreply.github.com>
2022-10-27 19:24:33 +0900

    add:Saturday

---
+++ archive/atcoder.jp/abc267/abc267_a/Main.php
@@ -0,0 +1,22 @@
+<?php
+  switch (trim(fgets(STDIN))) {
+            case 'Monday':
+                echo 5;
+                break;
+      		
+      		case 'Tuesday':
+                echo 4;
+                break;
+            
+      		case 'Wednesday':
+                echo 3;
+                break;
+      
+      		case 'Thursday':
+                echo 2;
+                break;
+      
+      		case 'Friday':
+                echo 1;
+                break;
+        }
